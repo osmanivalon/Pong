b0e61ee312b604c7b8d6c5fc04cec7c0eac83ad7
Osmani Valon <Valon.Osmani@automationx.com>
2021-12-07 12:10:22 +0100

    Add prints for build file

--- build-windows/build.bat
+++ build-windows/build.bat
@@ -1,3 +1,18 @@
 cd /d %~dp0
+
+@echo off
+color B
+echo
+echo =============================================================
+echo                  Build Preparation
+echo =============================================================
+
+
 cmake -DCMAKE_TOOLCHAIN_FILE=C:\views\vcpkg\scripts\buildsystems\vcpkg.cmake ..
+
+color A
+echo 
+echo =============================================================
+echo                     Generations
+echo =============================================================
 cmake --build .
